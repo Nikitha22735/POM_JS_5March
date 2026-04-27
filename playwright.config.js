91fee22fc8631ecdc8854029d34acdfb99dc0796
Nikitha22735 <nikitha.thripuram@gmail.com>
2026-04-27 20:00:00 +0530

    demo on commitPR

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   expect:{
     timeout: 40000,
   },
-  timeout:50000,
+  timeout:70000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
